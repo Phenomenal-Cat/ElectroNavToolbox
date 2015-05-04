e0363713aa8a2863ec8561fbd5147095228f71ae
Aidan Murphy <murphyap@mail.nih.gov>
2015-05-04 18:05:19 -0400

    Updating contact info

--- EN_About.m
+++ EN_About.m
@@ -5,7 +5,7 @@
 % completed.
 %
 % ELECTRONAV TOOLBOX
-% Developed by Aidan Murphy (murphyap@mail.nih.gov)
+% Developed by Aidan Murphy
 % © Copyleft 2014, GNU General Public License
 %==========================================================================
 
@@ -13,12 +13,10 @@ function FigHandle = EN_About(Loading, temp)
     if nargin ==0
         Loading = 0;
     end
-    ElectroNavVersion = 1.0;
-  	ContactEmail = 'murphyap@mail.nih.gov';
-    GithubSite = 'https://github.com/MonkeyGone2Heaven/ElectroNavToolbox';
+    Info = EN_Version;
     FigBackground = [1 1 1]*0.75;
     LogoFile = 'ElectroNavLogo1.png';
-    FigLogo = imread(LogoFile,'BackgroundColor',FigBackground);
+    FigLogo = imread(Info.LogoFile,'BackgroundColor',FigBackground);
     [a b FigLogoAlpha] = imread(LogoFile); 
     LogoSize = size(FigLogo);
     FigAboutRect = [500 500 500 300];                
@@ -43,21 +41,21 @@ function FigHandle = EN_About(Loading, temp)
     Text1 = 'A MATLAB® Toolbox for MRI-guided Electrode Navigation';
     Text2 = sprintf(['Version %.1f, developed by Aidan Murphy %s Copyleft 2014\n',...
                     'Section on Cognitive Neurophysiology and Imaging, NIMH\n\n',...
-                    'Contact:\n\nDownload:'],ElectroNavVersion, char(169));
+                    'Contact:\n\nDownload:'],Info.Version, char(169));
   	TextAxH(1) = axes('Units','pixels','position',[FigMargin,FigMargin-10,FigAboutRect(3)-(2*FigMargin),FigAboutRect(4)*0.38],'visible','off');
   	TextH(1) = text(0,1,Text1,'FontWeight','bold','FontUnits','pixels','FontSize',15,'HorizontalAlignment','left','VerticalAlignment','top');
     TextAxH(2) = axes('Units','pixels','position',[FigMargin,40,FigAboutRect(3)-(2*FigMargin),FigAboutRect(4)*0.18],'visible','off');
     TextH(2) = text(0,1,Text2,'FontUnits','points','FontSize',12,'HorizontalAlignment','left','VerticalAlignment','top');
-    labelStr{1} = sprintf('<html><center><a href="">%s', ContactEmail);                     % Set label for e-mail button
+    labelStr{1} = sprintf('<html><center><a href="">%s@%s', Info.ContactEmail, Info.ContactDomain);    	% Set label for e-mail button
     labelStr{2} = sprintf('<html><center><a href="">GitHub'); 
-    cbStr{1} = sprintf('web([''mailto:'',''%s'']);', ContactEmail);                       	% Set link to e-mail
-    cbStr{2} = sprintf('web(''%s'');', GithubSite);                                             
-    hButton(1) = uicontrol('string',labelStr{1},'pos',[110,45,160,20],'callback',cbStr{1});	% Create push button to send e-mail
+    cbStr{1} = sprintf('web([''mailto:'',''%s@%s'']);', Info.ContactEmail, Info.ContactDomain);       	% Set link to e-mail
+    cbStr{2} = sprintf('web(''%s'');', Info.GithubSite);                                             
+    hButton(1) = uicontrol('string',labelStr{1},'pos',[110,45,160,20],'callback',cbStr{1});             % Create push button to send e-mail
     hButton(2) = uicontrol('string',labelStr{2},'pos',[110,20,160,20],'callback',cbStr{2}); 
     
-    if Loading == 1                                                                         % If input 'Loading' is 1...
+    if Loading == 1                                                                                     % If input 'Loading' is 1...
         TextAxH(3) = axes('Units','pixels','position',[FigMargin,FigAboutRect(4)-(2*FigMargin),500,40],'visible','off');
         TextH(3) = text(0,1,'Loading...','FontWeight','bold','FontSize',20,'HorizontalAlignment','left','VerticalAlignment','top');
     end
-    drawnow;                                                                                % Display figure window
+    drawnow;                                                                                            % Display figure window
 end
---
+++ EN_Version.m
@@ -0,0 +1,15 @@
+%============================= EN_Version.m ===============================
+% This subfunction gets basic ElectroNav version information.
+%
+% ELECTRONAV TOOLBOX
+% Developed by Aidan Murphy
+% © Copyleft 2014, GNU General Public License
+%==========================================================================
+
+function Info = EN_Version()
+
+Info.Version = 1.0;
+Info.ContactEmail = 'murphyap';
+Info.ContactDomain = 'mail.nih.gov';
+Info.GithubSite = 'https://github.com/MonkeyGone2Heaven/ElectroNavToolbox';
+Info.LogoFile = 'ElectroNavLogo1.png';

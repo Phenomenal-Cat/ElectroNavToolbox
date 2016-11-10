dcaf7d50147a220cdd168a3f3a975b69e3db233e
Aidan Murphy <murphyap@mail.nih.gov>
2016-11-10 17:19:02 -0500

    Added MRI slice processing

--- ENT_GetSlice.m
+++ ENT_GetSlice.m
@@ -1,4 +1,4 @@
-function Slice = ENT_GetSlice(Subject, Dim, SlicePos)
+function Slice = ENT_GetSlice(Subject, Dim, SlicePos, ROIFiles)
 
 %========================== ENT_GetSlice.m ================================
 % This function returns slice images from an MRI volume for the specified
@@ -23,3 +23,14 @@ for n = 1:3
 end
 Indx{Dim} = Slice.Origin(Dim) + SlicePos/Slice.Scale(Dim);
 Slice.Im = squeeze(nii.img(Indx{1},Indx{2},Indx{3}));
+
+if exist('ROIFiles','var')
+    ROI = load_nii(ROIFiles);
+    Slice.ROI.Origin  = ROI.hdr.hist.originator(1:3);
+    Slice.ROI.Scale   = ROI.hdr.dime.pixdim(2:4);
+    for n = 1:3
+        Indx{n} = 1:size(ROI.img, n);
+    end
+    Indx{Dim}       = Slice.ROI.Origin(Dim) + SlicePos/Slice.ROI.Scale(Dim);
+    Slice.ROI.Im    = squeeze(ROI.img(Indx{1},Indx{2},Indx{3}));
+end
